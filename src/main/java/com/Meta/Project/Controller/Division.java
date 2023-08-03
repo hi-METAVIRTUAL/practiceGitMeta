7dc6989fed9f7a2c2dc293931875324a41bf23dc
jiyeong08 <a01086283142@gmail.com>
2023-08-03 22:21:34 +0900

    20230803-jy-average-2

--- src/main/java/com/Meta/Project/Controller/Division.java
+++ src/main/java/com/Meta/Project/Controller/Division.java
@@ -16,17 +16,6 @@ public class Division {
         System.out.println("나눗셈의 결과는 "+result+"입니다.");
 
 
-        System.out.println("나누고자하는 숫자를 띄워서 입력해주세요: ");
-
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
-
-        double result;
-
-        result = (double)num1/num2;
-
-        System.out.println("나눗셈 결과: " + result);
-
 
 
     }
