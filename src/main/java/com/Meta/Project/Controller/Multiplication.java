0f7a523d29970d0f0c7466d2a1752458f48ae0a9
Gray-Grazer <cleit8307@gmail.com>
2023-08-04 14:21:28 +0900

    case 4 and 9 update

--- src/main/java/com/Meta/Project/Controller/Multiplication.java
+++ src/main/java/com/Meta/Project/Controller/Multiplication.java
@@ -9,14 +9,14 @@ public class Multiplication {
         Scanner sc1 = new Scanner(System.in);
 
             int result1 = 0;
-            System.out.print("첫번째 수를 입력: ");
+            System.out.print("첫번째 수를 입력 : ");
             int input1 = sc1.nextInt();
             sc1.nextLine();
-            System.out.print("두번째 수를 입력: ");
+            System.out.print("두번째 수를 입력 : ");
             int input2 = sc1.nextInt();
 
             result1 = input1 * input2;
-            System.out.println("곱셈 결과: " + result1);
+            System.out.println("곱셈 결과 : " + result1);
 
 
     }
